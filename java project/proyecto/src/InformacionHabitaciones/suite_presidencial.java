c985c643dbbbbbec3e0118fe7da69aac14ecfff1
LuckyK <88355964+aL0ver@users.noreply.github.com>
2022-08-15 13:17:44 -0500

    Update and rename suite_precidencial.java to suite_presidencial.java

--- java project/proyecto/src/InformacionHabitaciones/suite_presidencial.java
+++ java project/proyecto/src/InformacionHabitaciones/suite_presidencial.java
@@ -1,11 +1,11 @@
 package InformacionHabitaciones;
 
 
-public class suite_precidencial extends equipamiento_habitacion {
+public class suite_presidencial extends equipamiento_habitacion {
 
     private boolean servicio_24h;
 
-    public suite_precidencial(boolean _televisor, boolean _nevera, boolean _minibar, boolean _servicio24h) {
+    public suite_presidencial(boolean _televisor, boolean _nevera, boolean _minibar, boolean _servicio24h) {
         
         super(_televisor, _nevera, _minibar);
         servicio_24h = _servicio24h;
